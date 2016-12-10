546bdef44ad14394885c96d7489c655869abcd15
Manuel Cepeda <manuelcepeda@MacBook-Pro-de-Manuel-2.local>
2016-12-09 21:19:11 -0300

    changed teacher_signup without underscore

--- mp/urls.py
+++ mp/urls.py
@@ -24,9 +24,9 @@ from front.views import (
 
 
 urlpatterns = [
-    url(r"^account/signup/teacher/$", newteacherprofile, name="teacher_signup"), 
     url(r"^$", TemplateView.as_view(template_name="homepage.html"), name="home"),
     url(r"^admin/", include(admin.site.urls)),
+    url(r"^account/signup/teacher/$", SignupTeacherView.as_view(), name="teacher_signup"), 
     url(r"^newteacherprofile/$", newteacherprofile, name="new_teacher_profile"),      
     url(r"^account/", include("account.urls")), #pinax urls
 
